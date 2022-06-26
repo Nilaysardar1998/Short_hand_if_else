2c2c66fc0f23076f4442c33b2f02d12b8bdaa66d
NILAY SARDAR <nilay.sardar04121998@gmail.com>
2022-06-26 13:02:22 +0530

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+//c short-hand if-else (ternary operator)
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 20;
+    
+    (x < y) ? printf("Hello, World!\n") : printf("Lol");
+
+    return 0;
+}
